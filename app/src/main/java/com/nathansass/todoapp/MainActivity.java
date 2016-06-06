cf20315b3737f19e2b5eeb6e49e24ca2be46e3d4
Nathan Sass <nsass711@gmail.com>
2016-06-06 10:15:51 -0400

    Able to display a list and then add an item to that list TODO deleting items

--- app/src/main/java/com/nathansass/todoapp/MainActivity.java
+++ app/src/main/java/com/nathansass/todoapp/MainActivity.java
@@ -2,12 +2,41 @@ package com.nathansass.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
+    EditText etEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+
+        etEditText = (EditText) findViewById(R.id.etEditText);
+    }
+
+    public void populateArrayItems() {
+        todoItems = new ArrayList<String>();
+        todoItems.add("Item1");
+        todoItems.add("Item2");
+        todoItems.add("Item3");
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+
+    }
+
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
     }
 }
